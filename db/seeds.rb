6c75c368769f9d499dc4ad4850f8e242a02f4a5a
kathyebel <kathyebel@me.com>
2018-06-04 11:38:08 -0400

    update seed file with user.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,14 @@
+# frozen_string_literal: true
+
+@user = User.create!(email: 'post@test.com', password: 'password',
+                    password_confirmation: 'password',
+                    first_name: 'Jon', last_name: 'Snow')
+
+puts '1 user created'
+
 100.times do |post|
-  Post.create(date: Date.today,
+  Post.create(user_id: @user.id,
+              date: Date.today,
               rationale: "#{post} rationale")
 end
 
