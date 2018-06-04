09eb19c2050a3dc68477deaf554ddc928f5521e9
kathyebel <kathyebel@me.com>
2018-06-03 22:51:35 -0400

    Implement creating posts.

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -11,4 +11,21 @@ describe 'Post' do
       expect(page).to have_content('Posts')
     end
   end
+
+  describe 'creation' do
+    before do
+      visit new_post_path
+    end
+
+    it 'should reach new form page' do
+      expect(page.status_code).to eq 200
+    end
+
+    it 'should create a new post with form' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: 'Some rationale'
+      click_on 'Save'
+      expect(page).to have_content('Some rationale')
+    end
+  end
 end
